2d730c90bc47a56f198720d0841632f395c342fe
Carlos Antonio <carlosabcj1@gmail.com>
2025-01-22 16:06:50 -0300

    app flex

--- appflex.js
+++ appflex.js
@@ -12,7 +12,7 @@ function calcular() {
     gasolina = frmFlex.inputGasolina.value
     //console.log(etanol)
     //console.log(gasolina)
-    if (etanol < 0,7 * gasolina) {
+    if (etanol < 0.7 * gasolina) {
         document.getElementById('status').src="img/etanol.png"
     } else {
         document.getElementById('status').src="img/gasolina.png"
